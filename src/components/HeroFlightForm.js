8c29f4ff7f884ce8e513d6a842be47fe7f7d615f
jamievullo <jvullo@protonmail.com>
2020-02-19 14:38:44 -0500

    Added handle change and handle submit, working on fetch

--- src/components/HeroFlightForm.js
+++ src/components/HeroFlightForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 export default class HeroFlightForm extends Component {
 
@@ -15,7 +16,26 @@ export default class HeroFlightForm extends Component {
         disabled: false,
     }
 
+    handleChange = (event) => {
+        // const target = event.target;
+        // const value = target.type === 'checkbox' ? target.checked : target.value;
+        // const name = target.name;
 
+        this.setState({
+            [flyingFrom]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+  
+        console.log(event.target);
+  
+        this.setState({
+            disabled: true
+        });
+            //fetch
+    }
 
     render() {
         return (
@@ -23,23 +43,32 @@ export default class HeroFlightForm extends Component {
                  {/* style={{ backgroundColor: "grey", height: "30%", width: "83%"}} */}
                 <div className='hero-flight-form'>
                 <Form onSubmit={this.handleSubmit}>
-                    <Form.Group style={{ float: "left", marginRight: "80px" }}>
+                        <Form.Row>
+                    <Form.Group>
+                        <Col>
                         <Form.Label>Flying From</Form.Label>
                         <Form.Control id="flying-from" type="text" placeHolder="city or airport" value={this.state.flyingFrom} onChange={this.handleChange} />
+                    </Col>
                     </Form.Group>
-                    <Form.Group style={{ float: "left", marginRight: "80px" }}>
+                    <Form.Group>
+                    <Col>
                         <Form.Label>Flying To</Form.Label>
                         <Form.Control id="flyingTo" type="text" placeHolder="city or airport" value={this.state.flyingTo} onChange={this.handleChange} />
+                    </Col>
                     </Form.Group>
-                    
-                    <Form.Group style={{ float: "left", marginRight: "80px" }}>
+                    <Form.Group>
+                        <Col>
                         <Form.Label>Departing</Form.Label>
                         <Form.Control id="departing" type="text" placeHolder="mm/dd/yyy" value={this.state.departing} onChange={this.handleChange} />
+                        </Col>
                     </Form.Group>
-                    <Form.Group style={{ float: "left", marginRight: "80px" }}>
+                    <Form.Group>
+                        <Col>
                         <Form.Label>Returning</Form.Label>
                         <Form.Control id="returning" type="text" placeHolder="mm/dd/yyy" value={this.state.returning} onChange={this.handleChange} />
+                        </Col>
                     </Form.Group>
+                    </Form.Row>
                 </Form>            
                 <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
                         Get Deals
