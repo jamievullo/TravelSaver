c751ce43c51a344bb26ed1b60152fad58d06ed78
jamievullo <jvullo@protonmail.com>
2020-03-02 13:14:40 -0500

    Store created.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TravelSaverNavbar from './components/TravelSaverNavbar';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import tsReducer from './reducers/tsReducer'
 
 export default function App() {
 
-  const store = createStore()
+  const store = createStore(tsReducer)
 
   return (
     <Provider store={store}>
