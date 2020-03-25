6c7ac35e7567d94f06c328ee53ceecb2416fa63b
jamievullo <jvullo@protonmail.com>
2020-03-25 11:31:38 -0400

    Formatted dates for flightdisplay.

--- src/components/FlightResultsDisplay.js
+++ src/components/FlightResultsDisplay.js
@@ -3,8 +3,15 @@ import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ts from '../assets/images/travelsaver2.png'
+import moment from 'moment';
 
 export default function FlightResultsDisplay(props) {
+
+    function dateFormat(date) {
+        let unformattedDate = moment(date, 'YYYY-MM-DDTHH:mm:ss.sss')
+        let formattedDate = unformattedDate.format('MM-DD-YYYY [Time:] hh:mm a')
+        return formattedDate
+    }
 
     return (
         <div>
@@ -23,8 +30,8 @@ export default function FlightResultsDisplay(props) {
                             <Card.Text>Price: ${props.flight.price}</Card.Text>
                             </Card.Title>
                             {/* trying to conditionally render default props if not present in response */}
-                            {props.flight.local_departure && <Card.Text>Departure: {props.flight.local_departure}</Card.Text>} 
-                            {props.flight.local_arrival && <Card.Text>Arrival: {props.flight.local_arrival}</Card.Text>}                                                        
+                            {props.flight.local_departure && <Card.Text>Departure: {dateFormat(props.flight.local_departure)}</Card.Text>} 
+                            {props.flight.local_arrival && <Card.Text>Arrival: {dateFormat(props.flight.local_arrival)}</Card.Text>}                                                        
                             <Card.Text as="h6">Click Here to Book Now: <a href={props.flight.deep_link} target="_blank" rel="noopener noreferrer">${props.flight.price}</a></Card.Text>                       
                         </Col>
                     </Row>    
