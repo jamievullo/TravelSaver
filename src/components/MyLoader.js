876314f1fea5cbc5df64f7a726b38d54f99a1413
jamievullo <jvullo@protonmail.com>
2020-03-28 09:24:56 -0400

    Changed margins for skeleton.

--- src/components/MyLoader.js
+++ src/components/MyLoader.js
@@ -5,15 +5,16 @@ function MyLoader() {
     return (
         <ContentLoader
             speed={2}
-            width={900}
+            width={1000}
             height={250}
-            viewBox="0 0 900 300"
+            viewBox="0 0 1000 300"
             backgroundColor="#f3f3f3"
             foregroundColor="#ecebeb"
+            marginBottom={20}
         >
             <rect x="50" y="50" rx="5" ry="5" width="250" height="250" />
-            <rect x="310" y="50" rx="4" ry="4" width="700" height="17" />
-            <rect x="310" y="75" rx="4" ry="4" width="700" height="17" />
+            <rect x="310" y="50" rx="4" ry="4" width="800" height="17" />
+            <rect x="310" y="75" rx="4" ry="4" width="800" height="17" />
             <rect x="310" y="110" rx="3" ry="3" width="100" height="12" />
             <rect x="310" y="140" rx="3" ry="3" width="50" height="12" />
             <rect x="310" y="170" rx="3" ry="3" width="800" height="12" />
@@ -23,4 +24,4 @@ function MyLoader() {
     )
 }
 
-export default MyLoader
+export default MyLoader;
