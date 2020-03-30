0b803ce7dab8024eeb2fa08a09e203ea92e73ce0
jamievullo <jvullo@protonmail.com>
2020-03-30 07:43:23 -0400

    Adjusted styling of skeleton for flight loader.

--- src/components/MyFlightLoader.js
+++ src/components/MyFlightLoader.js
@@ -12,12 +12,12 @@ function MyFlightLoader() {
             foregroundColor="#ecebeb"
             marginbottom={20}
         >
-            <rect x="300" y="50" rx="5" ry="5" width="200" height="200" />
-            <rect x="510" y="50" rx="4" ry="4" width="800" height="20" />
-            <rect x="510" y="75" rx="4" ry="4" width="150" height="20" />
-            <rect x="510" y="120" rx="3" ry="3" width="400" height="12" />
-            <rect x="510" y="150" rx="3" ry="3" width="350" height="12" />
-            <rect x="510" y="190" rx="3" ry="3" width="300" height="12" />
+            <rect x="280" y="50" rx="5" ry="5" width="200" height="200" />
+            <rect x="490" y="50" rx="4" ry="4" width="800" height="25" />
+            <rect x="490" y="80" rx="4" ry="4" width="100" height="25" />
+            <rect x="490" y="120" rx="3" ry="3" width="350" height="12" />
+            <rect x="490" y="150" rx="3" ry="3" width="300" height="12" />
+            <rect x="490" y="190" rx="3" ry="3" width="250" height="12" />
         </ContentLoader>
     )
 }
